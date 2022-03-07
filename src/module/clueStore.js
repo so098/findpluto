b2b869e07e7c921ef74813773d8bb194979a6604
so098 <szerohan@gmail.com>
2022-03-07 18:03:43 +0900

    feat: Add symbol array

--- src/module/clueStore.js
+++ src/module/clueStore.js
@@ -2,7 +2,9 @@ import create from "zustand";
 
 const clueStore = create((set) => ({
   clues: [],
+  symbols: [],
   setClue: (choiceClue) => set(() => ({ clues: choiceClue })),
+  setSymbols: (choiceSymbol) => set(() => ({ symbols: choiceSymbol })),
 }));
 
 export default clueStore;
