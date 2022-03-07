786d1da3c1b0dffccbf51149cbb30c630bc2ee4d
so098 <szerohan@gmail.com>
2022-03-07 18:04:18 +0900

    refactor: Change modal description

--- src/feature/findJohn/DescriptionModal.js
+++ src/feature/findJohn/DescriptionModal.js
@@ -6,9 +6,8 @@ function DescriptionModal({ modalOpen, closeModal }) {
   return (
     <Modal open={modalOpen} close={closeModal} header="존의 위치를 찾으세요.">
       명왕성에 도착하였습니다.
-      <br /> 이제 존에게 얻은 단서를 통해 존의 위치를 추정하여 클릭해보세요.
-      <br />
-      존의 위치를 클릭하면 그 위치로 이동합니다.
+      <br /> 존에게 얻은 단서를 바탕으로 명왕성의 위치를 추적하였습니다
+      <br /> 존이 있을 좌표를 클릭하여 명왕성 내부로 들어가십시오
     </Modal>
   );
 }
