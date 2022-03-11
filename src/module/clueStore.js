5f01e1e732e4d174138192ab62bbc2745890209a
so098 <szerohan@gmail.com>
2022-03-11 10:54:56 +0900

    feat: Add clueindex johnposition store

--- src/module/clueStore.js
+++ src/module/clueStore.js
@@ -3,8 +3,13 @@ import create from "zustand";
 const clueStore = create((set) => ({
   clues: [],
   symbols: [],
+  clueIndex: 0,
+  johnPosition: [],
   setClue: (choiceClue) => set(() => ({ clues: choiceClue })),
   setSymbols: (choiceSymbol) => set(() => ({ symbols: choiceSymbol })),
+  setClueIndex: (clueLocationNumber) =>
+    set(() => ({ clueIndex: clueLocationNumber })),
+  setJohnPosition: (position) => set(() => ({ johnPosition: position })),
 }));
 
 export default clueStore;
