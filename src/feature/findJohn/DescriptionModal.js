fdc85689e80753ad32fce90744e0359ef2347180
so098 <szerohan@gmail.com>
2022-03-13 07:05:50 +0900

    refactor: Change description text

--- src/feature/findJohn/DescriptionModal.js
+++ src/feature/findJohn/DescriptionModal.js
@@ -12,7 +12,7 @@ function DescriptionModal({ modalOpen, closeModal, check }) {
     >
       명왕성에 도착하였습니다.
       <br /> 존에게 얻은 단서를 바탕으로 명왕성의 위치를 추적하였습니다
-      <br /> 존이 있을 좌표를 클릭하여 명왕성 내부로 들어가십시오
+      <br /> 존이 있을 좌표를 클릭하여 명왕성 내부로 들어가세요
     </Modal>
   );
 }
