7cfebb8fd7b2f21dbea00e985dcef2c55ca5445d
so098 <szerohan@gmail.com>
2022-03-13 01:07:37 +0900

    refactor: Change insidePluto naming

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes, useNavigate } from "react-router-dom";
 
 import FindJohn from "./feature/findJohn/FindJohn";
 import Home from "./feature/home/Home";
+import InsidePluto from "./feature/insidePluto/InsidePluto";
 import Tutorial from "./feature/tutorial/Tutorial";
 function App() {
   const navigate = useNavigate();
@@ -22,6 +23,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/tutorial" element={<Tutorial />} />
         <Route path="/findJohn" element={<FindJohn />} />
+        <Route path="/InsidePluto" element={<InsidePluto />} />
       </Routes>
     </>
   );
