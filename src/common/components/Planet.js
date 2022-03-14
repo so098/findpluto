0363568208855401ef3a2e05f5088776c86774d5
so098 <szerohan@gmail.com>
2022-03-15 00:22:17 +0900

    refactor: Add proptypes

--- src/common/components/Planet.js
+++ src/common/components/Planet.js
@@ -2,15 +2,16 @@ import React, { useRef } from "react";
 
 import { OrbitControls } from "@react-three/drei";
 import { useLoader, useFrame } from "@react-three/fiber";
+import PropTypes from "prop-types";
 import * as THREE from "three";
 
-function Planet({ image, refNone }) {
+const Planet = ({ image, animationNone }) => {
   const planetAnimation = useRef();
-  const animationNone = useRef();
+  const animationNoneRef = useRef();
   const texture = useLoader(THREE.TextureLoader, image);
 
   useFrame(() => {
-    if (!refNone) {
+    if (!animationNone) {
       planetAnimation.current.rotation.x += 0.0004;
       planetAnimation.current.rotation.y += 0.0035;
     }
@@ -18,13 +19,21 @@ function Planet({ image, refNone }) {
 
   return (
     <>
-      <mesh ref={refNone ? animationNone : planetAnimation} enableZoom={false}>
+      <mesh
+        ref={animationNone ? animationNoneRef : planetAnimation}
+        enableZoom={false}
+      >
         <OrbitControls />
         <sphereGeometry args={[2.5, 32, 32]} />
         <meshStandardMaterial map={texture} />
       </mesh>
     </>
   );
-}
+};
+
+Planet.propTypes = {
+  image: PropTypes.string,
+  animationNone: PropTypes.string,
+};
 
 export default Planet;
