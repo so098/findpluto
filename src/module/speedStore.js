5d2acc18a99e45c739541dd721ffa5b945b261c7
so098 <szerohan@gmail.com>
2022-03-17 17:36:33 +0900

    refactor: Delete file

--- src/module/speedStore.js
+++
@@ -1,8 +0,0 @@
-import create from "zustand";
-
-const speedStore = create((set) => ({
-  speed: 30,
-  setSpeed: (speedNumber) => set(() => ({ speed: speedNumber })),
-}));
-
-export default speedStore;
