da3749f1dc9c926e790f13ee0a073bc76b3e90f8
so098 <szerohan@gmail.com>
2022-03-17 17:35:05 +0900

    refactor: Add testcase

--- src/spec/Modal.spec.js
+++ src/spec/Modal.spec.js
@@ -1,17 +1,24 @@
 import React from "react";
 
-import { render } from "@testing-library/react";
-
 import Modal from "../common/components/Modal";
-
+import { customRender } from "../setupTests";
 describe("<Modal/>", () => {
+  const open = true;
   const close = jest.fn();
-  it("Modal component를 render한다", () => {
-    render(<Modal />);
+  const check = "확인";
+
+  beforeEach(() => {
+    const { getByText } = customRender(<Modal open={open}>test</Modal>);
+    expect(getByText("test")).toBeTruthy();
   });
 
-  it("모달 close시 모달이 닫혀야 한다", () => {
-    const { getByTestId } = render(<Modal close={close}>black</Modal>);
-    expect(getByTestId("modal")).toBeInTheDocument();
+  it("Modal component를 render한다.", () => {
+    customRender(<Modal />);
+  });
+
+  it("Modal button을 클릭하면 닫힌다..", () => {
+    const { getByTestId } = customRender(<Modal close={close} check={check} />);
+    const smallEl = getByTestId("closeButton");
+    expect(smallEl).toBeInTheDocument();
   });
 });
